15dacd4ee12d23291023176904a79f06d7c18896
Gracie_hmr <zueva150245@yandex.ru>
2023-12-18 10:59:53 +0800

    Добавьте файлы проекта.

---
+++ Portfolio/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ Portfolio/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public int Rating { get; set; }
+    }
+}

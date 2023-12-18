2a3364409d051d3178a4669e1032b48057ea74d8
Gracie_hmr <zueva150245@yandex.ru>
2023-12-18 15:28:33 +0800

    Настройка поставщика БД

--- Portfolio/Models/DataDbContext.cs
+++ Portfolio/Models/DataDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace Portfolio.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Portfolio.Models
 {
-    public class DataDbContext
+    public class DataDbContext : DbContext
     {
+        public DataDbContext(DbContextOptions<DataDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().ToTable("Posts");
+            modelBuilder.Entity<Project>().ToTable("Projects");
+            modelBuilder.Entity<Review>().ToTable("Reviews");
+        }
     }
 }
